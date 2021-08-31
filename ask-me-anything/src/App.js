f155970a26d4e758445482785e01a558fd8f3475
caitlinhamilton <caitlinhamiltonmail@gmail.com>
2021-08-31 20:29:46 +0100

    added navbar

--- ask-me-anything/src/App.js
+++ ask-me-anything/src/App.js
@@ -2,14 +2,38 @@ import React from 'react';
 import Home from './Home.js'
 import AdminBoard from './Admin.js'
 import {getTableData} from './api';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+export default function App() {
   return (
-    <div>
-      {/* <Home getTableData={getTableData}></Home> */}
-      <AdminBoard getTableData={getTableData}></AdminBoard>
-    </div>
+    <Router>
+      <div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Home</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="admin">Admin</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+        <Switch>
+          <Route path="/admin">
+            <AdminBoard getTableData={getTableData}></AdminBoard>
+          </Route>
+          <Route path="/">
+            <Home getTableData={getTableData}></Home> 
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
